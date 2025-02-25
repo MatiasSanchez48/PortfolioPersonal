26f400c813bb1becfe47b4f2c5a1541aa5feef47
Matias Sanchez <sanchezmati48@gmail.com>
2025-02-25 02:42:34 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/portfolio/personal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.portfolio_personal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:portfolio_personal/app/app.dart';
+import 'package:portfolio_personal/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
